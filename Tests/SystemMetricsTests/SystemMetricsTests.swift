2bc0c931a4dd929d2818d1707d29e2e1885d1cc6
Vladimir Kukushkin <kukushechkin@mac.com>
2025-11-12 16:11:59 +0000

    rename to SystemMetricsMonitor

--- Tests/SystemMetricsTests/SystemMetricsTests.swift
+++ Tests/SystemMetricsTests/SystemMetricsTests.swift
@@ -16,7 +16,7 @@ import CoreMetrics
 import Dispatch
 import Foundation
 import MetricsTestKit
-import SystemMetrics
+import SystemMetricsMonitor
 import Testing
 
 @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
